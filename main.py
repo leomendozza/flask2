5cee3fa13f9fdcc0ab2431d721d473066086dadc
leomendozza <97541734+leomendozza@users.noreply.github.com>
2024-02-07 12:37:58 -0300

    objeto request

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask ##importo desde la libreria flask 
+from flask import Flask, request ##importo desde la libreria flask 
 
 app = Flask(__name__) ##inicio la app
 
 @app.route("/home") ## declaro cual va a ser mi ruta desde el url (a esto es le llama endpoint)
 def hello():
-    return "hola"
+    user_ip_information = request.remote_addr ##el objeto request accede a la informfacion que esta enviando el cliente 
+    return f'Esta es la ip del cliente {user_ip_information}'
 
 app.run(debug = True) ##con esto corro la app y el debug sirve para poder actualizar sola la pagina
